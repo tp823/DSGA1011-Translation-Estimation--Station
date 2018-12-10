2930b8c3523c61f4484c9fb9d2a5b0dc331fda74
Alex <ams889@nyu.edu>
2018-12-09 19:18:09 -0500

    adding in hyperparam shell script

---
+++ code/run_RNNhyperparams.sh
@@ -0,0 +1,15 @@
+#python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-lr-001/' --batchSize 32
+
+#python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.01 --n_batch 200 --savePath '../model/rnn-vi-lr-01/' --batchSize 32
+
+python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.0001 --n_batch 200 --savePath '../model/rnn-vi-lr-0001/' --batchSize 32
+
+python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --p_dropOut 0 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-dr-0/' --batchSize 32
+
+python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --p_dropOut 0.3 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-dr-3/' --batchSize 32
+
+python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --p_dropOut 0.6 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-dr-6/' --batchSize 32
+
+#python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --p_dropOut 0 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-hs-100/' --batchSize 32
+
+#python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --p_dropOut 0.3 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-hs-200/' --batchSize 32
