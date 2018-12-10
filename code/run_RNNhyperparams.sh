56e9974d89e05faccdd15a923f3452e28d9dfde9
Alex Simonofff <ams889@nyu.edu>
2018-12-10 16:38:40 -0500

    fixing output path

--- code/run_RNNhyperparams.sh
+++ code/run_RNNhyperparams.sh
@@ -2,7 +2,7 @@
 
 #python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.01 --n_batch 200 --savePath '../model/rnn-vi-lr-01/' --batchSize 32
 
-#python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.0001 --n_batch 200 --savePath '../model/rnn-vi-lr-1/' --batchSize 32
+#python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.0001 --n_batch 200 --savePath '../model/rnn-vi-lr-0001/' --batchSize 32
 
 #python train.py --inputPath '../project_data/model_input/en-vi_v100K_emb300K/' --dataLang 'vi' --dataLength 50 --p_dropOut 0 --targetLength 50 --modelName 'RNNseq2seq' --n_iter 200 --lr 0.001 --n_batch 200 --savePath '../model/rnn-vi-dr-0/' --batchSize 32
 
